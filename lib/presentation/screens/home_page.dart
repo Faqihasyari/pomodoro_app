c9b010c443980ab4e19cb2a44db9e7cbbdc5e46d
faqihasyari <faqih8158@gmail.com>
2025-12-13 21:57:20 +0700

    delete command

--- lib/presentation/screens/home_page.dart
+++ lib/presentation/screens/home_page.dart
@@ -9,51 +9,58 @@ class MyHomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: bg,
-      body: Row(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Column(
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
               children: [
-                Text('jaksdjasndansdajdjansjandasndaalsndlnsdasnajsndn'),
-              ],
-            )),
-          Expanded(
-            flex: 2,
-            child: Column(
-              children: [
-                SizedBox(
-                  width: 300,
-                  height: 300,
-                  child: Stack(
+                Expanded(
+                  flex: 1,
+                  child: Column(
                     children: [
-                      CuteTodoPage(),
-                      // Positioned(
-                      //   left: 70,
-                      //   top: 250,
-                      //   child: Row(
-                      //     mainAxisAlignment: MainAxisAlignment.center,
-                      //     children: List.generate(
-                      //       12,
-                      //       (i) => Container(
-                      //         margin: const EdgeInsets.symmetric(horizontal: 4),
-                      //         width: 12,
-                      //         height: 12,
-                      //         decoration: BoxDecoration(
-                      //           color: Colors.black,
-                      //           borderRadius: BorderRadius.circular(100),
-                      //         ),
-                      //       ),
-                      //     ),
-                      //   ),
-                      // ),
+                      Text('jaksdjasndansdajdjansjandasndaalsndlnsdasnajsndn'),
+                    ],
+                  )),
+                Expanded(
+                  flex: 2,
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        width: 300,
+                        height: 300,
+                        child: Stack(
+                          children: [
+                            CuteTodoPage(),
+                            // Positioned(
+                            //   left: 70,
+                            //   top: 250,
+                            //   child: Row(
+                            //     mainAxisAlignment: MainAxisAlignment.center,
+                            //     children: List.generate(
+                            //       12,
+                            //       (i) => Container(
+                            //         margin: const EdgeInsets.symmetric(horizontal: 4),
+                            //         width: 12,
+                            //         height: 12,
+                            //         decoration: BoxDecoration(
+                            //           color: Colors.black,
+                            //           borderRadius: BorderRadius.circular(100),
+                            //         ),
+                            //       ),
+                            //     ),
+                            //   ),
+                            // ),
+                          ],
+                        ),
+                      ),
                     ],
                   ),
                 ),
               ],
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
