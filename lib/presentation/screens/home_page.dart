8d4b51fadad2baaecc1073a47a3eac2c61909518
faqihasyari <faqih8158@gmail.com>
2025-12-15 22:17:38 +0700

    add focus and rest c

--- lib/presentation/screens/home_page.dart
+++ lib/presentation/screens/home_page.dart
@@ -2,12 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:pomodoro_app/core/constants/color.dart';
 import 'package:pomodoro_app/core/widgets/bullet.dart';
 import 'package:pomodoro_app/core/widgets/todo_list.dart';
+import 'package:pomodoro_app/presentation/provider/pomodoro_provider.dart';
+import 'package:provider/provider.dart';
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
 
   @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final _focusController = TextEditingController();
+  final _testController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
+    final pomodoro = context.watch<PomodoroProvider>();
+    
     return Scaffold(
       backgroundColor: bg,
       body: Padding(
@@ -43,6 +55,7 @@ class MyHomePage extends StatelessWidget {
                 ),
               ],
             ),
+            // timer pomodoro
           ],
         ),
       ),
