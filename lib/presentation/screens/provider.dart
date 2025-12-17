c9936888636459daa778a19a060accb65a97ba0b
faqihasyari <faqih8158@gmail.com>
2025-12-17 17:14:12 +0700

    edit taskProvider

--- lib/presentation/screens/provider.dart
+++ lib/presentation/screens/provider.dart
@@ -1,41 +1,48 @@
 import 'package:flutter/foundation.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:pomodoro_app/core/models/task.dart';
-import 'package:uuid/uuid.dart'; 
 
 class TaskProvider with ChangeNotifier {
   final List<TaskModel> _tasks = [];
   final Box<TaskModel> _taskBox;
 
-  TaskProvider({required Box<TaskModel> taskBox}) : _taskBox = taskBox;
+ 
+  TaskProvider({required Box<TaskModel> taskBox}) : _taskBox = taskBox {
+    _loadTasks();
+  }
+
+  void _loadTasks() {
+    _tasks.clear(); 
+    _tasks.addAll(_taskBox.values); 
+    notifyListeners();
+  }
 
 
   List<TaskModel> get tasks => _tasks;
 
-  void addTask(String task) {
+  void addTask(String task) async {
     if (task.trim().isEmpty) return; 
 
     final newTask = TaskModel(
-      id: const Uuid().v4(), 
+      id: DateTime.now().millisecondsSinceEpoch.toString(), 
       task: task.trim(),
     );
 
-    _tasks.add(newTask);
-    notifyListeners(); 
+    await _taskBox.add(newTask);
+    _loadTasks();
   }
 
-  void toggleTask(String taskId) {
-    final taskIndex = _tasks.indexWhere((task) => task.id == taskId);
-    if (taskIndex != -1) {
-      _tasks[taskIndex] = _tasks[taskIndex].copyWith(
-        isCompleted: !_tasks[taskIndex].isCompleted,
-      );
-      notifyListeners();
-    }
+  Future<void> toggleTask(String taskId) async {
+    final taskIndex = _tasks.firstWhere((task) => task.id == taskId);
+    taskIndex.isCompleted = !taskIndex.isCompleted;
+    await taskIndex.save();
+    _loadTasks();
   }
 
-  void deleteTask(String taskId) {
-    _tasks.removeWhere((task) => task.id == taskId);
+  Future<void> deleteTask(String taskId) async {
+    final task = _tasks.firstWhere((task) => task.id == taskId);
+    await task.delete();
     notifyListeners();
   }
+  
 }
