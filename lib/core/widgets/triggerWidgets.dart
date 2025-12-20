0f06d7627212b7342ffb1d2a44845a905c2c5f28
faqihasyari <faqih8158@gmail.com>
2025-12-20 22:00:21 +0700

    add file

---
+++ lib/core/widgets/triggerWidgets.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/widgets.dart';
+
+class Triggerwidgets extends StatefulWidget {
+  const Triggerwidgets({super.key});
+
+  @override
+  State<Triggerwidgets> createState() => _TriggerwidgetsState();
+}
+
+class _TriggerwidgetsState extends State<Triggerwidgets> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: 
+      ),
+      child: ,
+    );
+  }
+}
