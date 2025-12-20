4135cef8ac8d9bac383c5f0c191a9f3168e5a615
faqihasyari <faqih8158@gmail.com>
2025-12-20 22:34:39 +0700

    dotted line for triggers container

--- lib/core/models/dotLine.dart
+++ lib/core/models/dotLine.dart
@@ -50,3 +50,55 @@ class _DottedLinePainter extends CustomPainter {
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
 }
+
+// buat trigger
+class DottedLineTriggers extends StatelessWidget {
+  final double height;
+  final Color color;
+
+  const DottedLineTriggers({
+    super.key,
+    this.height = 1,
+    this.color = Colors.grey,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 50),
+      child: CustomPaint(
+        size: Size(double.infinity, height),
+        painter: _DottedLinePainter(color),
+      ),
+    );
+  }
+}
+
+class _DottedLinePainterTriggers extends CustomPainter {
+  final Color color;
+
+  _DottedLinePainterTriggers(this.color);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final paint = Paint()
+      ..color = color
+      ..strokeWidth = size.height;
+
+    const dashWidth = 5;
+    const dashSpace = 4;
+    double startX = 0;
+
+    while (startX < size.width) {
+      canvas.drawLine(
+        Offset(startX, 0),
+        Offset(startX + dashWidth, 0),
+        paint,
+      );
+      startX += dashWidth + dashSpace;
+    }
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) => false;
+}
